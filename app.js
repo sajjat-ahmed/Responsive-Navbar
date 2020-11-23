d60bdd9c00d74956031a6b5911ac1c0de4dec649
sajjat-ahmed <ahm.sajjat@gmail.com>
2020-11-23 11:13:19 +0600

    Javascript responsive navbar

---
+++ app.js
@@ -0,0 +1,25 @@
+const navSlider = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forward ${index / 7 + 0}s`;
+            }
+        });
+
+    // Burger Animation
+    burger.classList.toggle('toggle');
+
+    }); 
+};
+
+navSlider();
